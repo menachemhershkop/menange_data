a7ec7f57cfefef810d4533b1d040fa906bbe2523
mendy hershkop <hers.mm@gmail.com>
2026-02-12 09:36:40 +0200

    create casic table

---
+++ src/compunent/Terrorist.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import list from '../db/data.json'
+import '../css/Terrorist.css'
+function Terrorist() {
+  return (
+    <div>
+            <table>
+                <tr>
+                <th>name</th>
+                <th>organization</th>
+                <th>attacks Count</th>
+                <th>status</th>
+                <th>relation To Israel Summary</th>
+                <th>image</th>
+        </tr>
+      {list.map((key, a)=>{
+          if (key.imageUrl == null) {
+              key.imageUrl = 'public/mostefa.png'
+            }
+            return (
+                <tr key={a}>
+                    <td>{key.name}</td>
+                    <td>{key.organization}</td>
+                    <td>{key.attacksCount}</td>
+                    <td>{key.status}</td>
+                    <td>{key.relationToIsraelSummary}</td>
+            <td><img src={key.imageUrl}></img></td>
+            </tr>
+        
+            )
+      })}
+      </table> 
+    </div>
+  )
+}
+
+export default Terrorist
